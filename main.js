c3b94e2d1415328afab72c21c63f8089e41225e5
oeasy1412 <140829724+oeasy1412@users.noreply.github.com>
2023-08-19 22:55:41 +0800

    Update main.js

--- main.js
+++ main.js
@@ -17,17 +17,17 @@ function audiojs() {
   js.play();
 }
 function rock(){
-  document.getElementById("myChoice").innerHTML="<img src=\"images/rock.png\"/>";
+  document.getElementById("myChoice").innerHTML="<img src=\"images/rock.png\" style=\"width:130px\"/>";
   judge("rock");
   lastHumanChoice = "rock";
 } 
 function scissors(){
-  document.getElementById("myChoice").innerHTML="<img src=\"images/scissors.png\"/>";
+  document.getElementById("myChoice").innerHTML="<img src=\"images/scissors.png\" style=\"width:130px\"/>";
   judge("scissors");
   lastHumanChoice = "scissors";
 }
 function paper(){
-  document.getElementById("myChoice").innerHTML="<img src=\"images/paper.png\"/>";
+  document.getElementById("myChoice").innerHTML="<img src=\"images/paper.png\" style=\"width:130px\"/>";
   judge("paper");
   lastHumanChoice = "paper";
 } 
@@ -103,9 +103,6 @@ function judge(myChoice){
   }
   document.getElementById("result").innerHTML="第"+grade+"关,积分："+score;
   if(grade>=5){
-    document.getElementById("demo").innerHTML="继续游戏，聆听可莉的祝福";
-  }
-  if(grade>=6){
     document.getElementById("result").innerHTML="NB,总通关啦！"+score;
   }
 } 
@@ -114,47 +111,47 @@ function go(){
 function computerChoice(){
   r = Math.random()*3;
   if(r<1){
-    document.getElementById("computerChoice").innerHTML="<img src='images/rock.png'>";
+    document.getElementById("computerChoice").innerHTML="<img src='images/rock.png' style=\"width:130px\">";
     return "rock";
   }
   else if(r<2){
-    document.getElementById("computerChoice").innerHTML="<img src='images/scissors.png'>";
+    document.getElementById("computerChoice").innerHTML="<img src='images/scissors.png' style=\"width:130px\">";
     return "scissors";
   }
   else{
-    document.getElementById("computerChoice").innerHTML="<img src='images/paper.png'>";
+    document.getElementById("computerChoice").innerHTML="<img src='images/paper.png' style=\"width:130px\">";
     return "paper";
   }
 } 
 function onlyRocker(){
   document.getElementById("computerName").innerHTML="电脑大锤";
-  document.getElementById("computerChoice").innerHTML="<img src='images/rock.png'>";
+  document.getElementById("computerChoice").innerHTML="<img src='images/rock.png' style=\"width:130px\">";
   return "rock";
 } 
 function learnFromHuman(){
   document.getElementById("computerName").innerHTML="模仿大帝";
-  document.getElementById("computerChoice").innerHTML="<img src='images/"+lastHumanChoice+".png'>";
+  document.getElementById("computerChoice").innerHTML="<img src='images/"+lastHumanChoice+".png' style=\"width:130px\">";
   return lastHumanChoice;
 }  
 function winnerAgain(){
   document.getElementById("computerName").innerHTML="赢了还出";
   if(lastWinner=="com"){
-    document.getElementById("computerChoice").innerHTML="<img src='images/"+lastComputerChoice+".png'>";
+    document.getElementById("computerChoice").innerHTML="<img src='images/"+lastComputerChoice+".png' style=\"width:130px\">";
     return lastComputerChoice;
   }
   var temp = computerChoice();
-  document.getElementById("computerChoice").innerHTML="<img src='images/"+ temp +".png'>";
+  document.getElementById("computerChoice").innerHTML="<img src='images/"+ temp +".png' style=\"width:130px\">";
   return temp;
 } 
 function loseChange(){
   document.getElementById("computerName").innerHTML="输了就换";
   if(lastWinner=="human"){
     var temp = getResultExclude(lastComputerChoice);
-    document.getElementById("computerChoice").innerHTML="<img src='images/"+temp+".png'>";
+    document.getElementById("computerChoice").innerHTML="<img src='images/"+temp+".png' style=\"width:130px\">";
     return temp;
   }
   temp = computerChoice();
-  document.getElementById("computerChoice").innerHTML="<img src='images/"+ temp +".png'>";
+  document.getElementById("computerChoice").innerHTML="<img src='images/"+ temp +".png' style=\"width:130px\">";
   return temp;
 } 
 function getResultExclude(exclusion){
